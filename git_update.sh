1bef6b6225d918a5e44a94c35bfbd09d82290dc5
drshark95 <drshark@snu.ac.kr>
2021-05-27 20:47:16 +0900

    챗봇 업데이트(restart_restfulapiserver.sh 실행파일 추가 업데이트)

--- git_update.sh
+++ git_update.sh
@@ -3,5 +3,5 @@
 echo "git 업데이트"
 
 git add .
-git commit -m "챗봇 업데이트 24 (restart_restfulapiserver.sh 실행파일 추가 업데이트)"
+git commit -m "챗봇 업데이트(restart_restfulapiserver.sh 실행파일 추가 업데이트)"
 git push origin master
